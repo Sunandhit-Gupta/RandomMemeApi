a60b68bd1c0e0a3e7faa71b5f4347d4422b72c82
Sunandhit-Gupta <sunandhitgupta@gmail.com>
2022-10-03 02:54:23 +0530

    Added Likes,Title,authorName,subredditName and improved looks

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ async function getData(){
 
 function show(){
 getData().then(data => {
+    let ups = data.ups;
+    let title = data.title;
+    let author = data.author;
+    let postLink = data.postLink;
+    let subreddit = data.subreddit;
     let a = data.url;
-    element = document.getElementById("ig");
-    element.src = a ;
+    
+    image = document.getElementById("ig");
+    image.src = a;
+
+    subrdt = document.getElementById("subreddit");
+    subrdt.innerHTML = "Subreddit : "+subreddit;
+
+    lkno = document.getElementById("lkno");
+    lkno.innerHTML = "Likes : "+ ups;
+
+    ather = document.getElementById("author");
+    ather.innerHTML = author;
+
+    tile = document.getElementById("title");
+    tile.innerHTML =  title;
+
+    anchorpost=document.getElementById("anchorpost");
+    anchorpost.href = postLink;   
 })
 }
 show();
